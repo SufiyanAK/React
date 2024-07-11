899a78ca0b680c164f1d41a5e575cfca4ff5bf16
SufiyanAK <Sufiyankhattak14@gmail.com>
2024-07-11 07:08:00 +0500

    completed password generator

--- 05-useEffect-useRef-useCallback/src/App.jsx
+++ 05-useEffect-useRef-useCallback/src/App.jsx
@@ -1,28 +1,82 @@
-import { useState } from 'react'
+import { useState, useEffect, useCallback, useRef } from 'react'
 
 function App() {
   const [passwordLength, setPasswordLength] = useState(8)
+  const [isCharAllowed, setIsCharAllowed] = useState(true)
+  const [isNumberAllowed, setIsNumberAllowed] = useState(false)
+  const [password, setPassword] = useState('')
+  const passwordRef = useRef(null)
 
-  const handleChange = (e) => {
-    setPasswordLength(e.target.value)
-  }
+  // GENERATING PASSWORD
+  const passwordGen = useCallback(() => {
+    let pass = '';
+    let str = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+
+    if (isNumberAllowed) {
+      str += '0123456789';
+    }
+
+    if (isCharAllowed) {
+      str += '!@#$%&*~/|(){}[]';
+    }
+
+    for (let i = 1; i <= passwordLength; i++) {
+      let char = Math.floor(Math.random() * str.length + 1)
+      pass += str.charAt(char)
+    }
+
+    setPassword(pass)
+
+  }, [passwordLength, isNumberAllowed, isCharAllowed, setPassword])
+
+  const copyPasswordTOClip = useCallback(() => {
+    passwordRef.current?.select()
+    window.navigator.clipboard.writeText(password)
+  }, [password])
+
+
+  // USE EFFECT HOOK TO GENERATE ON EVERY CHANGE ON THE DEPENDENCIES
+  useEffect(() => {
+    passwordGen();
+  }, [passwordLength, isNumberAllowed, isCharAllowed, passwordGen])
 
   return (
     <>
-      <div className='font-mono w-full h-screen flex flex-col justify-center items-center'>
-        <h1 className='text-5xl font-bold'>Password Generator</h1>
-        <div className='bg-black rounded-md px-5 py-3'>
-          <div className='bg-white rounded-md flex justify-between gap-2'>
-            <input className='flex-1 text-xl p-2 outline-black' type="text" name="" id="" />
-            <button className='bg-green-500 px-3 py-2 rounded-md text-white font-bold'>Copy</button>
+      <div
+        className='font-mono w-full h-screen flex flex-col gap-y-4 justify-center items-center bg-slate-500'>
+        <h1
+          className='text-5xl font-bold'>Password Generator</h1>
+        <div
+          className='bg-black rounded-md px-5 py-4'>
+          <div
+            className='bg-white rounded-md flex justify-between gap-2'>
+            <input
+              className='flex-1 text-xl p-2 outline-none' value={password} readOnly type="text" ref={passwordRef} />
+            <button
+              className='bg-green-500 hover:bg-green-700 cursor-pointer px-3 py-2 rounded-md text-white font-bold duration-200'
+              onClick={copyPasswordTOClip}
+            >
+              Copy
+            </button>
           </div>
-          <div className='mt-4 text-white flex items-center gap-x-4'>
-            <input onChange={handleChange} type="range" min={8} max={20} minLength={8} maxLength={20} />
-            <div>Length: {passwordLength}</div>
-            <input type="checkbox" />
-            <div>Character</div>
-            <input type="checkbox" />
-            <div>Number</div>
+          <div
+            className='mt-4 text-white flex items-center gap-x-4'>
+            <input
+              onChange={(e) => { setPasswordLength(e.target.value) }}
+              type="range" min={8} max={20} />
+            <label>Length: {passwordLength}</label>
+            <input
+              type="checkbox" id='charInput'
+              defaultChecked={isCharAllowed}
+              onChange={() => { setIsCharAllowed(c => !c) }}
+            />
+            <label className='cursor-pointer' htmlFor='charInput' >Character</label>
+            <input
+              type="checkbox" id='numberInput'
+              defaultChecked={isNumberAllowed}
+              onChange={() => { setIsNumberAllowed(n => !n) }}
+            />
+            <label className='cursor-pointer' htmlFor='numberInput' >Number</label>
           </div>
         </div>
       </div>
